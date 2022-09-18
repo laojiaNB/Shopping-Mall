37f3cfd6523a95410039ef3db9c62b2b1d087726
yourname <3036659204@qq.com>
2022-09-18 22:36:24 +0800

    缓存起来

---
+++ pages/center/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content u-font-16" >
+	我的
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	 
+</style>
